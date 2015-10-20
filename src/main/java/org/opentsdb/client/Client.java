a2283307c8dadc171a31413a94f4a4f10dc73ca7
David_jin <gdweijin2008@163.com>
2015-10-20 10:10:25 +0800

    测试3
    测试3

--- src/main/java/org/opentsdb/client/Client.java
+++ src/main/java/org/opentsdb/client/Client.java
@@ -9,6 +9,8 @@ public interface Client {
 
 	// weijin add test 
 	// 2015-10-20 second test
+	// 第3次修改
+	public final static Integer GIT=0;
 	public final static String POST_API = "/api/put";
 
 	/**
