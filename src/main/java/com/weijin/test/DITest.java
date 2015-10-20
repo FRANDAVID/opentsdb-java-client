b743c8595b7d85b1e88ccb2ba3559ad35edd36f8
David_jin <gdweijin2008@163.com>
2015-10-20 14:15:21 +0800

    增加一个错误的语句

--- src/main/java/com/weijin/test/DITest.java
+++ src/main/java/com/weijin/test/DITest.java
@@ -3,6 +3,7 @@ package com.weijin.test;
 public class DITest {
 
 	public static void main(String[]args){
-		System.out.println("test");
+		System.out.println("test2");
+		system.out.println(222);
 	}
 }
